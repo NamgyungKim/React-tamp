baa7cd98ec7aa24fca90cf637a4277f49e9cca62
NamgyungKim <namgyung.kim@gmail.com>
2024-04-22 02:21:52 +0900

    add: 컴포넌트 파일 추가

---
+++ src/components/home/LearnMoreSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LearnMoreSection = () => {
+  return <div>Learn More Section</div>;
+};
+
+export default LearnMoreSection;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,18 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import MainSection from "../components/home/MainSection";
+import ProductSection from "../components/home/ProductSection";
+import LearnMoreSection from "../components/home/LearnMoreSection";
+
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <MainSection />
+      <ProductSection />
+      <LearnMoreSection />
+      <Footer />
+    </div>
+  );
 };
 export default Home;
